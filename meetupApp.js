979c6401df52177c0eb2bfacdc4c34bf37842b3f
Erin Linley <elinley1@gmail.com>
2018-04-03 14:53:33 -0400

    Working Call

--- meetupApp.js
+++ meetupApp.js
@@ -13,41 +13,41 @@ function initMap() {
                 lng: position.coords.longitude
             };
 
-        onClick( pos );
+            onClick(pos);
+            dropPin(pos);
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-        console.log('pos:  ', pos);
-    }, function () {
-        handleLocationError(true, infoWindow, map.getCenter());
-    });
-} else {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+            console.log('pos:  ', pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
 
-    handleLocationError(false, infoWindow, map.getCenter());
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
+function gotIt(response){
+    console.log('response: ', response);
 }
-
 function onClick(pos) {
+
     $(".buttons").on("click", function () {
         console.log("Button clicked");
         let sportName = $(this).attr("data-name");
         console.log("sportName: ", sportName);
-        console.log("This: ", $(this));
+        // console.log("This: ", $(this));
         $.ajax({
             url: constructURL(sportName, pos),
-            method: "GET"
+            method: "GET",
         }).then(function (res) {
-            console.log("Response: ", res)
-            lat = res.city.lat;
-            lng = res.city.lon;
-            var url = res.events.link;
-            var eName = res.events.name;
-            console.log("Lat & lon: ", lat, lng);
-        });
+            console.log("Response: ", res);
+            loopResponse(res.results)
+        })
 
-        
+
     });
 }
 
@@ -60,41 +60,34 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
-
 function constructURL(sport, lat, lon) {
-    return qURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/topics.json?callback=gotIt&page=1?&key=34305b6a752276562604f306a51d76&sign=true&photo-host=public&page=20&text=" + sport + "&lat=" + pos.lat + "&lon=" + pos.lng
+    return qURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/topics.json?page=1?&key=34305b6a752276562604f306a51d76&sign=true&photo-host=public&page=20&text=" + sport + "&lat=" + pos.lat + "&lon=" + pos.lng
 };
+eName = " ";
+eURL = " ";
+
+function loopResponse (res){
+    res.forEach(function (r) {
+        let lat = events.group.lat;
+        let lon = events.group.lon;
+        let json = '{events.group.name}';
+        let eName = JSON.parse(json);
+        console.log("eName: ", eName);
+        let eURL = events.name.link;
+        console.log("Lat & Lon Loop: ", lat, lon);
+        dropPin();
+    });
+  
+};       
 
 
-//         let sportName = $(this).data("name");
-//         console.log(name)
-        // $.ajax({
-        //     url: constructURL(sportName, lat, lng),
-        //     method: "GET"
-        // }).then(function (res) {
-        //     lat = res.city.lat;
-        //     lng = res.city.lon;
-        //     var url = res.events.link;
-        //     var eName = res.events.name;
-        // });
-
-// });
-// }
- //     var sport = $(this).val(".sport-button"),
-    //     
-
-
-//         
-
-//             var markers = locations.map(function (location, i) {
-//                 return new google.maps.Marker({
-//                     position: location,
-//                     label: sport, eName, url
-//                 });
-//             });
-//             var markerCluster = new MarkerClusterer(map, markers,
-//                 { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-
-//            //This is where Kenny will loop through the lat and lon for each event and put the property value pairs in an array.
-//            // var location = []        
-//         })})
+function dropPin(lat, lon) {
+    var markers =  new google.maps.Marker({
+            position: lat, lon,
+            map: map,
+            label: eName, eURL
+        });
+    };
+  
+           //This is where Kenny will loop through the lat and lon for each event and put the property value pairs in an array.
+           // var location = []        
